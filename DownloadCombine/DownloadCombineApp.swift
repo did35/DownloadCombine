396f4d3e6b61e3919b109e73e4e113343c13a86f
Didier <did35@yahoo.com>
2024-05-07 09:04:45 +0200

    Initial Commit

---
+++ DownloadCombine/DownloadCombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  DownloadCombineApp.swift
+//  DownloadCombine
+//
+//  Created by Didier Delhaisse on 07/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct DownloadCombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
